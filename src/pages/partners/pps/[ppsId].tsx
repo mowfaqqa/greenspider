8446ee5d33eb1328746710d11feaece60072d391
muwaffaq <itzmuwaffaq@gmail.com>
2023-08-07 16:36:53 +0100

    implemented responsiveness for pps screen

--- src/pages/partners/pps/[ppsId].tsx
+++ src/pages/partners/pps/[ppsId].tsx
@@ -13,36 +13,49 @@ const PPSDetails = () => {
     <div className="lg:pb-[100px]">
       <Header />
       <div className="lg:max-w-[1440px] mx-auto lg:px-[75px] text-primary-main lg:mt-[130px]">
-        <div className="relative grid grid-cols-3 mt-[104px]">
+        <div className="relative grid grid-cols-3 mt-[104px] px-4">
           <div className="col-span-2 ">
             <div className=" flex items-center">
               <div className="bg-secondary-main h-[60px] w-[11px]" />
               <div className="mx-2">
-                <div className="lg:text-[34px] font-bold">
+                <div className="text-xl lg:text-[34px] font-bold">
                   {ppsDetail?.name}
                 </div>
-                <span className="lg:text-[26px] font-medium">
+                <span className="text-sm lg:leading-10 lg:text-[26px] font-medium">
                   {ppsDetail?.post}
                 </span>
               </div>
             </div>
-            <p className="font-medium text-lg mx-4 mt-[40px]">
+            <p className="font-medium text-lg text-justify w-[335px] lg:w-full mx-4 mt-[40px]">
               {ppsDetail?.desc}
             </p>
           </div>
-          <div className="flex justify-end">
-            <div className="green-bg bg-[#2FAC4E] w-[297px] h-[223px] rounded-[30px]" />
-            <div className="absolute lg:right-[0.04%] lg:top-[6.5%]">
-              <Image
-                src={ppsDetail?.img!}
-                width={275}
-                height={396}
-                alt="Bukar Hassan"
-              />
+          <div>
+            <div className="hidden lg:flex justify-end">
+              <div className="green-bg bg-[#2FAC4E] w-[297px] h-[223px] rounded-[30px]" />
+              <div className="absolute lg:right-[0.04%] lg:top-[6.5%]">
+                <Image
+                  src={ppsDetail?.img!}
+                  width={275}
+                  height={396}
+                  alt="pps detail"
+                />
+              </div>
+            </div>
+            <div className="flex lg:hidden justify-end">
+              <div className="green-bg bg-[#2FAC4E] w-[93px] h-[70px] rounded-[5px]" />
+              <div className="absolute lg:right-[0.04%] lg:top-[6.5%]">
+                <Image
+                  src={ppsDetail?.img!}
+                  width={86}
+                  height={124}
+                  alt="pps detail"
+                />
+              </div>
             </div>
           </div>
         </div>
-        <div className="font-medium text-lg mx-4 mt-[140px]">
+        <div className="font-medium text-lg w-[335px] lg:w-full mx-4 px-4 text-justify mt-5 lg:mt-[140px]">
           {ppsDetail?.further_desc}
         </div>
       </div>
--- src/pages/partners/pps/index.tsx
+++ src/pages/partners/pps/index.tsx
@@ -11,7 +11,7 @@ const PPS = () => {
       <Header />
       <div className="lg:max-w-[1440px] mx-auto lg:px-[75px] text-primary-main lg:mt-[130px]">
         {PPSData.map((data, index) => (
-          <div key={index} className="relative grid grid-cols-3 mt-[192px]">
+          <div key={index} className="relative grid grid-cols-3 mt-[76px] lg:mt-[192px] px-5">
             <div className="col-span-2 ">
               <div className=" flex items-center">
                 <div className="bg-secondary-main h-[60px] w-[11px]" />
@@ -22,7 +22,9 @@ const PPS = () => {
                   </span>
                 </div>
               </div>
-              <p className="font-medium text-lg mx-4 mt-[40px]">{data.summary}</p>
+              <p className="font-medium text-lg mx-4 w-[333px] lg:w-full mt-[60px] lg:mt-[40px]">
+                {data.summary}
+              </p>
               <div className="mt-5">
                 <button
                   type="button"
@@ -33,15 +35,28 @@ const PPS = () => {
                 </button>
               </div>
             </div>
-            <div className="flex justify-end">
-              <div className="green-bg bg-[#2FAC4E] w-[297px] h-[223px] rounded-[30px]" />
-              <div className="absolute lg:right-[0.04%] lg:top-[6.5%]">
-                <Image
-                  src={data.img}
-                  width={275}
-                  height={396}
-                  alt="Bukar Hassan"
-                />
+            <div>
+              <div className="hidden lg:flex justify-end">
+                <div className="green-bg bg-[#2FAC4E] w-[297px] h-[223px] rounded-[30px]" />
+                <div className="absolute lg:right-[0.04%] lg:top-[6.5%]">
+                  <Image
+                    src={data.img}
+                    width={275}
+                    height={396}
+                    alt="pps partner"
+                  />
+                </div>
+              </div>
+              <div className="flex lg:hidden justify-end">
+                <div className="green-bg bg-[#2FAC4E] w-[93px] h-[70px] rounded-[5px]" />
+                <div className="absolute lg:right-[0.04%] lg:top-[6.5%]">
+                  <Image
+                    src={data.img}
+                    width={86}
+                    height={124}
+                    alt="pps partner"
+                  />
+                </div>
               </div>
             </div>
           </div>
