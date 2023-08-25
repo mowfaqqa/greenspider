31131d5ab63bcaefbfa2051968f35416bc2b3237
muwaffaq <itzmuwaffaq@gmail.com>
2023-08-25 15:55:11 +0100

    adjusted responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,13 +3,13 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-[#F3F3F3] text-center py-12 mt-20">
-      <h4 className="text-black text-lg font-medium">
+      <h4 className="text-black text-sm md:text-lg font-medium">
         Green Spider Consult LTD
       </h4>
-      <p className="text-base text-black">
+      <p className="text-xs md:text-base text-black">
         No. 5 Leribe Close, off Kolda Street, Wuse 2 Abuja.
       </p>
-      <p className="text-base text-black">
+      <p className="text-xs md:text-base text-black">
         Email: greenspiderconsult@gmail.com
       </p>
     </div>
